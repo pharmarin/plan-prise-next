00342ede6a51aed90cc50464c623b850ca933f62
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-01-25 10:46:30 +0100

    👌 IMPROVE: Use hooks in FormikField

--- client/components/forms/inputs/FormikField.tsx
+++ client/components/forms/inputs/FormikField.tsx
@@ -1,4 +1,4 @@
-import { Field, FieldAttributes, FieldProps } from "formik";
+import { FieldAttributes, useField, useFormikContext } from "formik";
 import React from "react";
 
 const FormikField: React.FC<
@@ -13,30 +13,29 @@ const FormikField: React.FC<
   disableOnSubmit,
   displayErrors,
   label,
-  name,
-  ...formikProps
+  ...props
 }) => {
-  return (
-    <Field name={name} {...formikProps}>
-      {({ field, form, meta }: FieldProps) => (
-        <>
-          {React.Children.map(children, (child) => {
-            if (React.isValidElement(child)) {
-              return React.cloneElement(child, {
-                ...field,
-                disabled: (disableOnSubmit && form.isSubmitting) || disabled,
-                label,
-              } as React.HTMLProps<HTMLInputElement>);
-            }
+  const [field, meta] = useField(props);
+  const { isSubmitting } = useFormikContext();
 
-            return child;
-          })}
-          {displayErrors && meta.touched && meta.error && (
-            <div className="mt-1 text-xs text-red-600">{meta.error}</div>
-          )}
-        </>
+  return (
+    <>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, {
+            ...field,
+            ...props,
+            disabled: (disableOnSubmit && isSubmitting) || disabled,
+            label,
+          } as React.HTMLProps<HTMLInputElement>);
+        }
+
+        return child;
+      })}
+      {displayErrors && meta.touched && meta.error && (
+        <div className="mt-1 text-xs text-red-600">{meta.error}</div>
       )}
-    </Field>
+    </>
   );
 };
 
