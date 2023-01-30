5ba4505151f764ac9d055b95b958a03fefcc75dc
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-01-30 10:44:46 +0100

    🐛 FIX: Truncate Button text

--- client/components/forms/inputs/Button.tsx
+++ client/components/forms/inputs/Button.tsx
@@ -9,21 +9,21 @@ const ButtonBase: React.FC<
 > = ({ children, disabled, loading, ...props }) => {
   return (
     <button disabled={disabled || loading} {...props}>
-      {loading && <Spinner className="-ml-1 mr-3" />}
-      {children}
+      {loading && <Spinner className="-ml-1 mr-2" />}
+      <span className="truncate">{children}</span>
     </button>
   );
 };
 
 const Button = styled(ButtonBase, {
-  base: tw`inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-sm text-white items-center disabled:bg-gray-400 disabled:cursor-not-allowed focus:ring-teal-500 transition-colors`,
+  base: tw`inline-flex justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-sm text-white items-center disabled:bg-gray-400 disabled:cursor-not-allowed focus:ring-teal-500 transition-colors min-w-0`,
   variants: {
     color: {
       white: tw`border-gray-300 bg-white text-gray-700 hover:bg-gray-50`,
       primary: tw`bg-teal-500 hover:bg-teal-600 focus:ring-teal-600`,
       red: tw`bg-red-600 hover:bg-red-700 focus:ring-red-500`,
       gradient: tw`bg-gradient-to-r from-emerald-500 to-teal-500 disabled:cursor-not-allowed disabled:from-gray-500 disabled:to-slate-500`,
-      link: tw`bg-transparent p-0 font-normal text-teal-500`,
+      link: tw`bg-transparent p-0 font-normal text-teal-500 shadow-none`,
     },
   },
   defaultVariants: {
