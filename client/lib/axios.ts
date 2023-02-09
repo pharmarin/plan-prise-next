617c3239bd407441524203e80eae8d841ff62770
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-02-09 08:35:08 +0100

    👌 IMPROVE: Retry request if CSRF mismatch

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,4 +1,5 @@
-import Axios from "axios";
+import Axios, { AxiosError } from "axios";
+import { fetchCsrfCookie } from "lib/redux/auth/actions";
 
 const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 const API_PATH = process.env.NEXT_PUBLIC_API_PATH;
@@ -15,5 +16,19 @@ const axios = Axios.create({
   },
   withCredentials: true,
 });
+
+/**
+ * If server respond with code 419 (CSRF token mismatch), fetch a new cookie and send request again
+ */
+axios.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    if (error.response?.status === 419) {
+      await fetchCsrfCookie();
+
+      return axios(error.response.config);
+    }
+  }
+);
 
 export default axios;
