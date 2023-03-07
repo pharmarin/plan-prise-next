f9a94a030a1e76f84f5d357114cda0dd717950c8
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-03-07 22:34:15 +0100

    🐛 FIX: Cannot update component while rendering another

--- client/components/navigation/ClientTitle.tsx
+++ client/components/navigation/ClientTitle.tsx
@@ -2,15 +2,18 @@
 
 import { useNavigationDispatch } from "@/components/NavigationContextProvider";
 import { NavigationActionKind } from "@/types";
+import { useEffect } from "react";
 
 const ClientTitle: React.FC<{ title: string }> = ({ title }) => {
   const dispatch = useNavigationDispatch();
 
-  if (!dispatch) {
-    throw new Error("Dispatch n'est pas disponible hors contexte");
-  }
+  useEffect(() => {
+    if (!dispatch) {
+      throw new Error("Dispatch n'est pas disponible hors contexte");
+    }
 
-  dispatch({ type: NavigationActionKind.SET_TITLE, payload: title });
+    dispatch({ type: NavigationActionKind.SET_TITLE, payload: title });
+  });
 
   return null;
 };
