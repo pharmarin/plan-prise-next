c5d58697ed9aa5f8959cfe522012a83dbf33bb2e
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-03-23 16:06:19 +0100

    📦 NEW: Add prisma connection test

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,40 +1,26 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
   root: true,
-  extends: [
-    "next",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
+  extends: ["next", "prettier"],
+  ignorePatterns: [
+    "**/*.config.js",
+    "**/*.setup.js",
+    "**/*.config.cjs",
+    "**/*rc.js",
   ],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "error",
-      { prefer: "type-imports", fixStyle: "inline-type-imports" },
-    ],
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      { checksVoidReturn: { attributes: false } },
-    ],
-    "@typescript-eslint/no-unsafe-argument": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/restrict-template-expressions": "off",
-  },
-  ignorePatterns: ["**/*.config.js", "**/*.config.cjs", "**/*rc.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     project: ["./tsconfig.json"],
     tsconfigRootDir: __dirname,
   },
+  plugins: ["testing-library"],
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+  ],
 };
 
 module.exports = config;
