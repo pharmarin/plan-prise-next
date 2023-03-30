a48d7641711fb913c48d223843934aa430a8de3b
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-03-30 16:54:07 +0200

    👌 IMPROVE: Add titles

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,11 @@
 import AuthLayout from "@/app/(auth)/layout";
 import Home from "@/app/Home";
+import Title from "@/components/navigation/Title";
 
 const Index = () => {
   return (
     <AuthLayout>
+      <Title title="Bienvenue" />
       <Home />
     </AuthLayout>
   );
