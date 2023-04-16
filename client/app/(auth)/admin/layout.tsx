179d6489ae92cc750b0edb077a0b6d33fef24ad6
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-04-16 09:37:04 +0200

    🐛 FIX: Remove async RSC

--- client/app/(auth)/admin/layout.tsx
+++ client/app/(auth)/admin/layout.tsx
@@ -2,6 +2,7 @@ import AdminGuard from "@/components/guards/AdminGuard";
 import { type PropsWithChildren } from "react";
 
 const AdminLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  // @ts-expect-error Server Component
   return <AdminGuard>{children}</AdminGuard>;
 };
 export default AdminLayout;
