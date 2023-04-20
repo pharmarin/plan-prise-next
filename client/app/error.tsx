e84f9e3a26d6792fc220b69568040fe184bc6206
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-04-20 14:32:20 +0200

    📦 NEW: Add basic error handler

---
+++ client/app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Button from "@/components/forms/inputs/Button";
+
+const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+  return (
+    <div>
+      <h2 className="text-bold text-xl">Une erreur est survenue</h2>
+      <Button onClick={() => reset()}>Réessayer</Button>
+    </div>
+  );
+};
+
+export default Error;
