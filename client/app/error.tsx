4a3067e7b98fb335c2b80f59f3fe524b2a3ca605
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-04-21 12:29:10 +0200

    🐛 FIX: Remove unused variable

--- client/app/error.tsx
+++ client/app/error.tsx
@@ -2,7 +2,7 @@
 
 import Button from "@/components/forms/inputs/Button";
 
-const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+const Error = ({ reset }: { error: Error; reset: () => void }) => {
   return (
     <div>
       <h2 className="text-bold text-xl">Une erreur est survenue</h2>
