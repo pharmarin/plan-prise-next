5bbda8d036e103407244d73658a99c875061e949
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-05-08 19:40:27 +0200

    👌 IMPROVE: Remove acess to route test Route should be redirecting in all case if JWT absent or invalide

--- frontend/__tests__/backend.test.ts
+++ frontend/__tests__/backend.test.ts
@@ -6,10 +6,4 @@ test.describe("backend tests", () => {
 
     expect(page.url()).toBe(process.env.FRONTEND_URL + "/");
   });
-
-  test("should throw error on unanthorized request", async ({ page }) => {
-    const response = await page.goto(process.env.BACKEND_URL + "/plan");
-
-    expect(response?.status()).toBe(401);
-  });
 });
