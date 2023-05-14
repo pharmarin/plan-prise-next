f1b6c14410422fda932f02e73c4b463a48cc3493
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-05-14 16:04:21 +0200

    👌 IMPROVE: Use createWriteStream to save output

--- api-pharmaciens/download.ts
+++ api-pharmaciens/download.ts
@@ -1,22 +1,22 @@
-import { writeFile } from 'fs/promises';
-import * as path from 'path';
-import { Agent } from 'https';
-import fetch from 'node-fetch';
-import { Extract } from 'unzipper';
+import { createWriteStream } from "fs";
+import { Agent } from "https";
+import fetch from "node-fetch";
+import { ParseOne } from "unzipper";
 
 const httpsAgent = new Agent({
   rejectUnauthorized: false,
 });
 
 fetch(
-  'https://service.annuaire.sante.fr/annuaire-sante-webservices/V300/services/extraction/Porteurs_CPS_CPF',
+  "https://service.annuaire.sante.fr/annuaire-sante-webservices/V300/services/extraction/Porteurs_CPS_CPF",
   { agent: httpsAgent }
 ).then(
-  async (
-    response //response.body.pipe(Extract({ path: 'data' }))
-  ) =>
-    await writeFile(
+  async (response) =>
+    response.body
+      .pipe(ParseOne())
+      .pipe(createWriteStream("data/practitionners.txt"))
+  /* await writeFile(
       path.join('data', 'Porteurs_CPS_CPF.txt'),
       new TextDecoder('iso-8859-1').decode(await response.arrayBuffer())
-    )
+    ) */
 );
