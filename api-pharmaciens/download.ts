c773bfbad00fa8b6a0e3d244dac635e61f05602f
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-05-17 16:34:12 +0200

    📦 NEW: Parse csv and save values to json file

--- api-pharmaciens/download.ts
+++ api-pharmaciens/download.ts
@@ -1,22 +1,72 @@
-import { createWriteStream } from "fs";
+import { parse } from "csv-parse";
+import { writeFile } from "fs/promises";
 import { Agent } from "https";
 import fetch from "node-fetch";
 import { ParseOne } from "unzipper";
+
+type CSVRecord = {
+  rpps: string;
+  lastName: string;
+  firstName: string;
+  profession: string;
+};
+
+type ParsedRecord = {
+  rpps: number;
+  lastName: string;
+  firstName: string;
+  profession?: never;
+};
+
+const allowedHeader = {
+  "Identifiant PP": "rpps",
+  "Nom d'exercice": "lastName",
+  "Prénom d'exercice": "firstName",
+  "Code profession": "profession",
+};
 
 const httpsAgent = new Agent({
   rejectUnauthorized: false,
 });
 
+const parser = parse({
+  delimiter: "|",
+  columns: (header: string[]) =>
+    header.map((column) =>
+      Object.keys(allowedHeader).includes(column)
+        ? allowedHeader[column]
+        : undefined
+    ),
+});
+
 fetch(
   "https://service.annuaire.sante.fr/annuaire-sante-webservices/V300/services/extraction/Porteurs_CPS_CPF",
   { agent: httpsAgent }
-).then(
-  async (response) =>
+).then(async (response) => {
+  const jsonArray: ParsedRecord[] = await new Promise((resolve, reject) => {
+    const records: ParsedRecord[] = [];
+
     response.body
       .pipe(ParseOne())
-      .pipe(createWriteStream("data/practitionners.txt"))
-  /* await writeFile(
-      path.join('data', 'Porteurs_CPS_CPF.txt'),
-      new TextDecoder('iso-8859-1').decode(await response.arrayBuffer())
-    ) */
-);
+      .pipe(parser)
+      .on("error", (reason) => {
+        console.log("rejected: ", reason);
+        reject();
+      })
+      .on("readable", function () {
+        let record: CSVRecord;
+        while ((record = parser.read()) !== null) {
+          if (record.profession === "21") {
+            records.push({
+              firstName: record.firstName,
+              lastName: record.lastName,
+              rpps: Number(record.rpps),
+            });
+          }
+        }
+      })
+      .on("end", () => resolve(records));
+  });
+
+  await writeFile("data/practitionners.json", JSON.stringify(jsonArray));
+});
