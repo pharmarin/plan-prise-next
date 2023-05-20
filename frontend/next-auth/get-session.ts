5e05e5647b81036bdfe29c4c79028c4642cd277d
Marin ROUX <marin_maxfresh-and-good@hotmail.fr>
2023-05-20 10:18:30 +0200

    📦 NEW: Get trpc & next-auth to use route handlers

--- frontend/next-auth/get-session.ts
+++ frontend/next-auth/get-session.ts
@@ -1,20 +1,4 @@
 import { nextAuthOptions } from "@/next-auth/options";
-import type {
-  GetServerSidePropsContext,
-  NextApiRequest,
-  NextApiResponse,
-} from "next";
 import { getServerSession as $getServerSession } from "next-auth";
 
-type GetServerSessionContext =
-  | {
-      req: GetServerSidePropsContext["req"];
-      res: GetServerSidePropsContext["res"];
-    }
-  | { req: NextApiRequest; res: NextApiResponse };
-
-export const getServerSession = (ctx?: GetServerSessionContext) => {
-  return ctx
-    ? $getServerSession(ctx.req, ctx.res, nextAuthOptions)
-    : $getServerSession(nextAuthOptions);
-};
+export const getServerSession = () => $getServerSession(nextAuthOptions);
--- frontend/trpc/context.ts
+++ frontend/trpc/context.ts
@@ -2,10 +2,10 @@ import { getServerSession } from "@/next-auth/get-session";
 import prisma from "@/prisma";
 import type { UserSession } from "@/prisma/types";
 import type * as trpc from "@trpc/server";
-import type * as trpcNext from "@trpc/server/adapters/next";
+import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 
-export const createContext = async (ctx: trpcNext.CreateNextContextOptions) => {
-  const session = await getServerSession(ctx);
+export const createContext = async (ctx: FetchCreateContextFnOptions) => {
+  const session = await getServerSession();
 
   return { ...ctx, prisma, user: session?.user as UserSession | undefined };
 };
