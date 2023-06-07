2d515d58765523d1b1809a48811d527644cf10a8
pharmarin <15229104+pharmarin@users.noreply.github.com>
2023-06-07 15:07:41 +0200

    👌 IMPROVE: Move prettier config to root dir

--- frontend/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-/* @type {import('prettier').Options}*/
-const config = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-  tabWidth: 2,
-  tailwindConfig: "./tailwind.config.js",
-  useTabs: false,
-};
-
-module.exports = config;
