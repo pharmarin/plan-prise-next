fd2b6d91b2462f39324c03eb9646f99694f147db
pharmarin <15229104+pharmarin@users.noreply.github.com>
2023-06-07 15:04:25 +0200

    🐛 FIX: Remove no-html-link-for-pages warning

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 /** @type {import("eslint").Linter.Config} */
 const config = {
   extends: ["next", "prettier", "plugin:@typescript-eslint/recommended"],
@@ -9,6 +12,10 @@ const config = {
   ],
   reportUnusedDisableDirectives: true,
   rules: {
+    "@next/next/no-html-link-for-pages": [
+      "warn",
+      [path.join(__dirname, "../../frontend")],
+    ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
@@ -22,7 +29,6 @@ const config = {
       { prefer: "type-imports", fixStyle: "separate-type-imports" },
     ],
     "@typescript-eslint/return-await": "error",
-    "no-html-link-for-pages": "off",
     // Required by @typescript-eslint/return-await
     "no-return-await": "off",
   },
