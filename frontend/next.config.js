ccd987c1844959dc6523a5f16e360a1b0b8ae020
pharmarin <15229104+pharmarin@users.noreply.github.com>
2023-09-20 20:01:55 +0000

    🐛 FIX: Fix pdf not showing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
   },
   reactStrictMode: true,
   transpilePackages: ["@plan-prise/api-pharmaciens"],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: "raw-loader",
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
