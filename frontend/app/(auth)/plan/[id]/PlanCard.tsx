ffc059d11fdc794c4446fd0c29c383c0cf67ed4e
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2023-09-21 23:14:56 +0200

    👌 IMPROVE: Remove toggle PlanCard

--- frontend/app/(auth)/plan/[id]/PlanCard.tsx
+++ frontend/app/(auth)/plan/[id]/PlanCard.tsx
@@ -1,11 +1,8 @@
-"use client";
-
 import PlanCardBody from "@/app/(auth)/plan/[id]/PlanCardBody";
 import PlanCardHeader from "@/app/(auth)/plan/[id]/PlanCardHeader";
 import PlanCardUI from "@/app/(auth)/plan/[id]/_ui/PlanCardUI";
 import { trpc } from "@/trpc/client";
 import type { MedicamentIdentifier } from "@/types/medicament";
-import { useState } from "react";
 
 const PlanCard = ({
   medicamentId,
@@ -14,18 +11,11 @@ const PlanCard = ({
   medicamentId: string;
   removeMedic: (medicament: MedicamentIdentifier) => void;
 }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
   const [medicament] = trpc.medics.unique.useSuspenseQuery(medicamentId);
 
   return (
     <PlanCardUI>
-      <PlanCardHeader
-        medicament={medicament}
-        open={showDetails}
-        removeMedic={removeMedic}
-        toggle={() => setShowDetails((showDetails) => !showDetails)}
-      />
+      <PlanCardHeader medicament={medicament} removeMedic={removeMedic} />
       <PlanCardBody medicament={medicament} />
     </PlanCardUI>
   );
--- frontend/app/(auth)/plan/[id]/PlanCardHeader.tsx
+++ frontend/app/(auth)/plan/[id]/PlanCardHeader.tsx
@@ -5,63 +5,36 @@ import type {
   MedicamentIdentifier,
   MedicamentInclude,
 } from "@/types/medicament";
-import {
-  ChevronDownIcon,
-  ChevronUpIcon,
-  XMarkIcon,
-} from "@heroicons/react/20/solid";
+import { XMarkIcon } from "@heroicons/react/20/solid";
 import { twMerge } from "tailwind-merge";
 
 const PlanCardHeader = ({
   medicament,
-  open,
   removeMedic,
-  toggle,
 }: {
   medicament: MedicamentInclude;
-  open: boolean;
   removeMedic: (medicament: MedicamentIdentifier) => void;
-  toggle: () => void;
 }) => {
   return (
     <PlanCardHeaderUI
       buttons={
-        <>
-          <Button
-            className="space-x-1 rounded-full bg-white py-0 text-red-600"
-            color="red"
-            onClick={() =>
-              removeMedic({
-                id: medicament.id,
-                denomination: medicament.denomination,
-              })
-            }
-            outline
-            tabIndex={-1}
-          >
-            <small className="mr-auto">
-              Supprimer<span className="hidden sm:inline"> la ligne</span>
-            </small>
-            <XMarkIcon className="h-3 w-3" />
-          </Button>
-          <Button
-            className="space-x-1 rounded-full bg-white py-0 text-gray-600"
-            onClick={() => toggle()}
-            color="white"
-            outline
-            tabIndex={-1}
-          >
-            <small className="mr-auto">
-              {open ? "Masquer" : "Afficher"}
-              <span className="hidden sm:inline"> les détails</span>
-            </small>
-            {open ? (
-              <ChevronUpIcon className="h-3 w-3" />
-            ) : (
-              <ChevronDownIcon className="h-3 w-3" />
-            )}
-          </Button>
-        </>
+        <Button
+          className="space-x-1 rounded-full bg-white py-0 text-red-600"
+          color="red"
+          onClick={() =>
+            removeMedic({
+              id: medicament.id,
+              denomination: medicament.denomination,
+            })
+          }
+          outline
+          tabIndex={-1}
+        >
+          <small className="mr-auto">
+            Supprimer<span className="hidden sm:inline"> la ligne</span>
+          </small>
+          <XMarkIcon className="h-3 w-3" />
+        </Button>
       }
     >
       <div className="flex flex-grow flex-col">
