dcbe1d59f20f8f6a9d18e150ce101b13ebdd54e8
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2023-10-12 22:49:50 +0200

    🐛 FIX: Add missing posologie to settings validation schema

--- frontend/validation/plan.ts
+++ frontend/validation/plan.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
 export const planSettingsSchema = z.object({
   posos: z.object({
+    poso_lever: z.boolean().optional().default(false),
     poso_matin: z.boolean().optional().default(true),
     poso_10h: z.boolean().optional().default(false),
     poso_midi: z.boolean().optional().default(true),
