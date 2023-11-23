5c5cd9b46264ded8e338468ffa8b735ada95ab6b
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2023-11-23 14:34:28 +0100

    🐛 FIX: Improve error message on error boundary Fixes #533

---
+++ frontend/components/pages/Maintenance.tsx
@@ -0,0 +1,10 @@
+import ErrorSkeleton from "@/components/pages/ErrorSkeleton";
+
+const Maintenance = () => (
+  <ErrorSkeleton
+    title="Maintenance en cours"
+    subtitle="Veuillez réessayer plus tard"
+  />
+);
+
+export default Maintenance;
