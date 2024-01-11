30ddd8b7a0cc20ac837346712f282ee8f0eae1b6
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-01-11 14:36:15 +0100

    👌 IMPROVE: Adjust sentry sample rate

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -10,7 +10,7 @@ Sentry.init({
   dsn: "https://08dfd92ac31791d1bd25b1b2bb6303fa@o4506332907634688.ingest.sentry.io/4506332908814336",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
