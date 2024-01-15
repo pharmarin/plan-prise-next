2ad8b7feddab929dc3e812325a3e6ad400289e2b
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-01-15 13:07:19 +0100

    👌 IMPROVE: Disable Sentry on CI

--- frontend/sentry.edge.config.ts
+++ frontend/sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  enabled: process.env.NODE_ENV !== "development",
+  enabled: process.env.NODE_ENV !== "development" || !process.env.CI,
 
   dsn: "https://08dfd92ac31791d1bd25b1b2bb6303fa@o4506332907634688.ingest.sentry.io/4506332908814336",
 
