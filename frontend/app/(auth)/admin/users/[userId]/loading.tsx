784d570b04e9a6087eb6f79c0015fc4cb4283844
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-02-26 13:20:53 +0100

    🐛 FIX: Merge error

--- frontend/app/(auth)/admin/users/[userId]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingScreen from "@plan-prise/ui/components/pages/Loading";
-
-const AppLoading = () => <LoadingScreen />;
-
-export default AppLoading;
--- frontend/app/(auth)/admin/users/[userId]/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { routes } from "@/app/routes-schema";
-
-import Error404 from "@plan-prise/ui/components/pages/404";
-
-const UserNotFound = () => (
-  <Error404 title="Cet utilisateur est introuvable" returnTo={routes.users()} />
-);
-
-export default UserNotFound;
