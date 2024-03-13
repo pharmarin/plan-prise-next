0b86d90b6bc9c0223e69211840d8aec1964f0d19
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-03-13 13:43:28 +0100

    🐛 FIX: Auth tests broken by server actions

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,56 +1,98 @@
-import type { NextFetchEvent } from "next/server";
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { GUEST_ROUTES, routes } from "@/app/routes-schema";
 import { env } from "@/env.mjs";
-import type { NextRequestWithAuth } from "next-auth/middleware";
-import { withAuth } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
 
 import { signJWT } from "@plan-prise/api/utils/json-web-token";
-import { NEXT_AUTH_PAGES } from "@plan-prise/auth/config";
 
 const toLegacy = (path: string, destination?: string) =>
   env.BACKEND_URL + (destination ?? path);
 
-const withAuthMiddleware = withAuth(
-  async (request: NextRequestWithAuth) => {
+const redirectTo = (request: NextRequest, path: string) => {
+  const url = request.nextUrl.clone();
+  url.pathname = path;
+  return NextResponse.redirect(url);
+};
+
+export const middleware = async (request: NextRequest) => {
+  const pathname = request.nextUrl.pathname;
+  const token = await getToken({ req: request });
+
+  if (env.MAINTENANCE_MODE) {
+    return NextResponse.rewrite(new URL("/maintenance", request.url));
+  }
+
+  // LEGACY ROUTES
+
+  if (pathname.startsWith("/ajax") || pathname.startsWith("/calendrier")) {
+    const legacyUrl = toLegacy(pathname) + request.nextUrl.search;
     const requestHeaders = new Headers(request.headers);
 
-    if (request.nextauth.token) {
+    if (token) {
       requestHeaders.append(
         "Authorization",
-        await signJWT({ user_id: request.nextauth.token.user.id }),
+        await signJWT({ user_id: token.user.id }),
       );
     }
-
-    const legacyUrl =
-      toLegacy(request.nextUrl.pathname) + request.nextUrl.search;
 
     return NextResponse.rewrite(legacyUrl, {
       request: { headers: requestHeaders },
     });
-  },
-  { pages: { signIn: NEXT_AUTH_PAGES.signIn } },
-);
-
-export const middleware = (
-  request: NextRequestWithAuth,
-  event: NextFetchEvent,
-) => {
-  const pathname = request.nextUrl.pathname;
-  const legacyUrl = toLegacy(pathname) + request.nextUrl.search;
-
-  if (env.MAINTENANCE_MODE && !pathname.startsWith("/_next")) {
-    return NextResponse.rewrite(new URL("/maintenance", request.url));
   }
 
-  if (pathname.startsWith("/ajax") || pathname.startsWith("/calendrier")) {
-    return withAuthMiddleware(request, event);
-  } else if (
+  if (
     pathname.startsWith("/css") ||
     pathname.startsWith("/fonts") ||
     pathname.startsWith("/js") ||
     pathname.startsWith("/img") ||
     pathname.startsWith("/files")
   ) {
+    const legacyUrl = toLegacy(pathname) + request.nextUrl.search;
+
     return NextResponse.rewrite(legacyUrl);
   }
+
+  // GUEST ROUTES
+
+  if (
+    Object.values(GUEST_ROUTES).find((route) => (route as string) === pathname)
+  ) {
+    return token?.user.id
+      ? redirectTo(request, routes.home())
+      : NextResponse.next();
+  }
+
+  // ADMIN ROUTES
+
+  if (pathname.startsWith("/admin")) {
+    return token?.user.admin
+      ? NextResponse.next()
+      : redirectTo(request, routes.home());
+  }
+
+  // AUTH ROUTES
+
+  return token?.user.id
+    ? NextResponse.next()
+    : redirectTo(
+        request,
+        routes.login(
+          (pathname !== routes.home() && { search: { redirect: pathname } }) ||
+            undefined,
+        ),
+      );
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
