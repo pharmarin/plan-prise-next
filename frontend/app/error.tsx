a4dc744bd82930d5eba1d3d6e5ebfad8789a606b
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-03-14 11:58:37 +0100

    👌 IMPROVE: Add digest to error message

--- frontend/app/error.tsx
+++ frontend/app/error.tsx
@@ -6,7 +6,13 @@ import * as Sentry from "@sentry/nextjs";
 import { Button } from "@plan-prise/ui/button";
 import ErrorSkeleton from "@plan-prise/ui/components/pages/ErrorSkeleton";
 
-const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
@@ -14,7 +20,7 @@ const Error = ({ error, reset }: { error: Error; reset: () => void }) => {
   return (
     <ErrorSkeleton
       title="Une erreur est survenue"
-      subtitle={error.message || ""}
+      subtitle={`${error?.message} (${error?.digest})`}
       action={
         <div className="flex space-x-4">
           <Button onClick={() => reset()}>Réessayer</Button>
