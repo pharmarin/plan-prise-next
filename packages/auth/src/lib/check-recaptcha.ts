52b3e0f6c1f2114fbba3832d325a20d24dae0b18
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-03-19 13:57:26 +0100

    🐛 FIX: Restore checkRecaptcha mock

--- packages/auth/src/lib/check-recaptcha.ts
+++ packages/auth/src/lib/check-recaptcha.ts
@@ -3,15 +3,17 @@ import { env } from "../../env.mjs";
 const checkRecaptcha = async (
   gRecaptchaToken: string,
 ): Promise<number | undefined> =>
-  await fetch("https://www.google.com/recaptcha/api/siteverify", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: `secret=${env.RECAPTCHA_SECRET}&response=${gRecaptchaToken}`,
-  })
-    .then((reCaptchaRes) => reCaptchaRes.json())
-    .then((reCaptchaRes) => reCaptchaRes?.score as number | undefined)
-    .catch(() => undefined);
+  process.env.CI === "true"
+    ? 0.9
+    : await fetch("https://www.google.com/recaptcha/api/siteverify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `secret=${env.RECAPTCHA_SECRET}&response=${gRecaptchaToken}`,
+      })
+        .then((reCaptchaRes) => reCaptchaRes.json())
+        .then((reCaptchaRes) => reCaptchaRes?.score as number | undefined)
+        .catch(() => undefined);
 
 export default checkRecaptcha;
