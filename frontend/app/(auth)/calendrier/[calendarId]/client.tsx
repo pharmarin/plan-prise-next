02459c62af561929a602335672c667bae337f6d4
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-04-11 14:05:59 +0200

    📦 NEW: Load medicament in calendar

--- frontend/app/(auth)/calendrier/[calendarId]/client.tsx
+++ frontend/app/(auth)/calendrier/[calendarId]/client.tsx
@@ -6,17 +6,23 @@ import MedicamentSelect from "@/app/_components/select-medicament";
 import CalendarCardBody from "@/app/(auth)/calendrier/[calendarId]/card-body";
 import useCalendarStore from "@/app/(auth)/calendrier/state";
 import { isCuid } from "@paralleldrive/cuid2";
-import type { Calendar } from "@prisma/client";
+import type { Calendar, Medicament, PrincipeActif } from "@prisma/client";
 import { useShallow } from "zustand/react/shallow";
 
 import errors from "@plan-prise/errors/errors.json";
 import { useToast } from "@plan-prise/ui/shadcn/hooks/use-toast";
 
-const CalendarClient = ({ calendar }: { calendar: Calendar }) => {
+const CalendarClient = ({
+  calendar,
+  medicaments,
+}: {
+  calendar: Calendar;
+  medicaments?: (Medicament & { principesActifs: PrincipeActif[] })[];
+}) => {
   const { toast } = useToast();
 
   const data = useCalendarStore((state) => state.data);
-  const medicaments = Object.keys(data ?? {});
+  const medicamentIdArray = Object.keys(data ?? {});
 
   const { addMedic, removeMedic } = useCalendarStore(
     useShallow((state) => ({
@@ -34,13 +40,15 @@ const CalendarClient = ({ calendar }: { calendar: Calendar }) => {
 
   return (
     <div className="space-y-4">
-      {medicaments.map((medicId) => (
+      {medicamentIdArray.map((medicId) => (
         <Card
           key={`calendar_${calendar.id}_${medicId}`}
           medicamentId={medicId}
           medicamentData={
             isCuid(medicId)
-              ? undefined //plan.medics.find((medicament) => medicament.id === id)
+              ? (medicaments?.find(
+                  (medicament) => medicament.id === medicId,
+                ) as PP.Medicament.Include)
               : {
                   id: medicId,
                   denomination: medicId,
@@ -65,7 +73,7 @@ const CalendarClient = ({ calendar }: { calendar: Calendar }) => {
       ))}
       <MedicamentSelect
         onChange={(value) => {
-          if (medicaments.includes(value.id)) {
+          if (medicamentIdArray.includes(value.id)) {
             toast({
               title: errors.CALENDAR_MEDICAMENT_ALREADY_ADDED_ERROR,
               variant: "destructive",
--- frontend/app/(auth)/calendrier/[calendarId]/page.tsx
+++ frontend/app/(auth)/calendrier/[calendarId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import CalendarClient from "@/app/(auth)/calendrier/[calendarId]/client";
 import { routes } from "@/app/routes-schema";
 import { Navigation } from "@/app/state-navigation";
+import { isCuid } from "@paralleldrive/cuid2";
 
 import { getServerSession } from "@plan-prise/auth/get-session";
 import prisma from "@plan-prise/db-prisma";
@@ -15,13 +16,28 @@ const Calendar = async ({ params }: { params: unknown }) => {
       where: { displayId: calendarId, user: { id: session?.user.id } },
     });
 
+    const medicamentIdArray = Object.keys(calendar.data ?? {})
+      .map((medicId) => (isCuid(medicId) ? medicId : undefined))
+      .filter((medicId): medicId is string => Boolean(medicId));
+    const medicaments =
+      medicamentIdArray.length > 0
+        ? await prisma.medicament.findMany({
+            where: {
+              OR: Object.keys(calendar.data ?? {}).map((medicId) => ({
+                id: medicId,
+              })),
+            },
+            include: { principesActifs: true },
+          })
+        : undefined;
+
     return (
       <div>
         <Navigation
           title={`Calendrier n°${calendar.displayId}`}
           returnTo={routes.calendars()}
         />
-        <CalendarClient calendar={calendar} />
+        <CalendarClient calendar={calendar} medicaments={medicaments} />
       </div>
     );
   } catch (e) {
