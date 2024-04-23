d5bdf279bd5ece027c3e20e483829a00127c7664
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-04-23 14:27:19 +0200

    🐛 FIX: backdrop-blur-sm makes popover float

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { PropsWithChildren } from "react";
 import React from "react";
 import { GlobalProviders } from "@/app/providers";
-import background from "@/public/home-bg.jpg";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -13,8 +12,8 @@ const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
       <Analytics />
       <SpeedInsights />
       <body
-        className="flex min-h-screen w-screen flex-col bg-white/30  bg-cover px-2 backdrop-blur-sm"
-        style={{ backgroundImage: `url(${background.src})` }}
+        className="flex min-h-screen w-screen flex-col bg-white/30  bg-cover px-2" // backdrop-blur-sm"
+        /* style={{ backgroundImage: `url(${background.src})` }} */
       >
         <GlobalProviders>{children}</GlobalProviders>
       </body>
