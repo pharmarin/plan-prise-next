8dbfae35daefffdbf2ceac3afe6f187329edfdcc
Marin ROUX <15229104+pharmarin@users.noreply.github.com>
2024-04-23 14:31:15 +0200

    👌 IMPROVE: Restore blur background

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from "react";
 import React from "react";
 import { GlobalProviders } from "@/app/providers";
+import background from "@/public/home-bg.png";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -12,8 +13,8 @@ const RootLayout: React.FC<PropsWithChildren> = ({ children }) => {
       <Analytics />
       <SpeedInsights />
       <body
-        className="flex min-h-screen w-screen flex-col bg-white/30  bg-cover px-2" // backdrop-blur-sm"
-        /* style={{ backgroundImage: `url(${background.src})` }} */
+        className="flex min-h-screen w-screen flex-col bg-white/30  bg-cover px-2"
+        style={{ backgroundImage: `url(${background.src})` }}
       >
         <GlobalProviders>{children}</GlobalProviders>
       </body>
